59f2a8a5e8bbfa96fcd954ffc285870b451e71ed
Hosea54 <ejohhoseamwongwe@gmail.com>
2025-11-14 22:24:35 -0500

    Form structure

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,4 +1,49 @@
 const Newsletter = () => {
-  return <div>Newsletter</div>;
+  return (
+    <form className="form">
+      <h4 style={{ textAlign: "center", marginBottom: "2rem" }}>
+        our newsletter
+      </h4>
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="name"
+          id="name"
+          defaultValue="hosea"
+        />
+        <label htmlFor="name" className="form-label">
+          last name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="lastName"
+          id="lastname"
+          defaultValue="ejoh"
+        />
+        <label htmlFor="name" className="form-label">
+          email
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="email"
+          id="email"
+          defaultValue="test@test.com"
+        />
+      </div>
+      <button
+        type="submit"
+        className="btn btn-block"
+        style={{ marginTop: "0.5rem" }}
+      >
+        submit
+      </button>
+    </form>
+  );
 };
 export default Newsletter;
