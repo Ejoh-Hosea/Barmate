d14f31721eaa79b0a48f3a50fd3d061d6e579ee0
Hosea54 <ejohhoseamwongwe@gmail.com>
2025-11-16 21:03:35 -0500

    seachForm Structure

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,4 +1,22 @@
+import { Form, useNavigation } from "react-router-dom";
+
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+  return (
+    <div>
+      <Form className="form">
+        <input
+          type="search"
+          name="search"
+          className="form-input"
+          defaultValue="vodka"
+        />
+        <button type="submit" className="btn" disabled={isSubmitting}>
+          {isSubmitting ? "searching..." : "search"}
+        </button>
+      </Form>
+    </div>
+  );
 };
 export default SearchForm;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { useLoaderData } from "react-router-dom";
 import CocktailList from "../components/CocktailList";
+import SearchForm from "../components/SearchForm";
+
 const cocktailSearchUrl =
   "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
@@ -16,6 +18,7 @@ const Landing = () => {
 
   return (
     <>
+      <SearchForm />
       <CocktailList drinks={drinks} />
     </>
   );
